3d1846891c085e1aa3eb28655beb9fb92d0cdc48
Meftahbouchra <meftahbouchra.31@gmail.com>
2020-02-11 18:28:14 +0100

    cat

---
+++ app/src/main/java/com/bouchra/myapplicationechange/Categorie.java
@@ -0,0 +1,44 @@
+package com.bouchra.myapplicationechange;
+
+public enum Categorie {
+    VEHICULES(1, "Vèhicules"),
+    TELEPHONES(2, "Tèlèphones"),
+    AUTOMOBILES(3, "Automobiles"),
+    PIECESDETACHEES(4, "Pièces dètachèes"),
+    IMMOBILIER(5, "Immobilier"),
+    VETEMENTS(6, "Vetements"),
+    LIVRES(7, "Livres"),
+    ELETRONIQUEETELECTROMENAGER(8, "Eletronique et electromènager"),
+    ACCESSOIRESDEMODE(9, "Accessoires de mode"),
+    COSMETIQUESETBEAUTE(10, "Cosmetiques et Beautè"),
+    MAISONETFOUNITURES(11, "Maison et fournitures"),
+    LOISIRSETDEVERTISSEMENTS(12, "Loisirs et Dèvertissements"),
+    MATIEIAUXETEQUIPEMENTS(13, "Matiriaux et Equipements");
+
+    private int point;
+    private String nomCategorie;
+
+    public void setpoint(int point) {
+        this.point = point;
+    }
+
+    public int getpoint() {
+        return point;
+    }
+
+
+    public void setnomCategorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
+    }
+
+    public String getnomCategorie() {
+        return nomCategorie;
+    }
+
+    private Categorie(int point, String nomCategorie) {
+        this.point = point;
+        this.nomCategorie = nomCategorie;
+    }
+
+
+}
