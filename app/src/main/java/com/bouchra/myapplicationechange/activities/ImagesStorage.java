68559fa8a10ff0dea854ec9f82817d526f7624ec
Meftahbouchra <meftahbouchra.31@gmail.com>
2020-02-18 16:35:20 +0100

    fin tof

--- app/src/main/java/com/bouchra/myapplicationechange/activities/ImagesStorage.java
+++ app/src/main/java/com/bouchra/myapplicationechange/activities/ImagesStorage.java
@@ -3,9 +3,8 @@ package com.bouchra.myapplicationechange.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -19,136 +18,186 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.bouchra.myapplicationechange.R;
 import com.bouchra.myapplicationechange.adapters.BootomSheetDialogCamGall;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.List;
 
-import static android.os.Environment.getExternalStoragePublicDirectory;
+public class ImagesStorage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-public class ImagesStorage extends AppCompatActivity  implements AdapterView.OnItemSelectedListener , BootomSheetDialogCamGall.BottomSheetLIstener {
-    @Override
-    public void onButtonCliked(String text) {
-        Toast.makeText(this, ""+text, Toast.LENGTH_SHORT).show();
-    }
-
-    private static int PICK_IMAGE_REQUEST = 1;
-    String pathToFile;
-
-    private Button ch;
     private Button up;
+ StorageReference mStorageRef;
 
-    private ImageView img;
-    StorageReference mStorageRef;
-
-    public Uri mImageUri;
+    private static final String IMAGE_DIRECTORY = "/Echange d article";
+    private int GALLERY = 1, CAMERA = 2;
+    private ImageView imageview;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images_storage);
+        //StorageReference Fire base
         mStorageRef = FirebaseStorage.getInstance().getReference("Images");
-        ch = findViewById(R.id.button_choose_image);
         up = findViewById(R.id.button_upload);
-        img = findViewById(R.id.image_view);
-        //permission
-        if (Build.VERSION.SDK_INT >= 23) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
-        }
+
+//Spinner
         Spinner spinner = findViewById(R.id.spinner_cat);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.choix_categorie, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-//camera
-        ch.setOnClickListener(v -> {
-            FileChooser();
-
-        });
+        imageview = findViewById(R.id.image_view);
         // bottom shet
-          Button buttonOpenBottomSheet = findViewById(R.id.button_sheet);
-          buttonOpenBottomSheet.setOnClickListener(v -> {
-              BootomSheetDialogCamGall bottomsheet = new BootomSheetDialogCamGall();
-              bottomsheet.show(getSupportFragmentManager(),"exemplBottomsheet");
-          });
+        Button buttonOpenBottomSheet = findViewById(R.id.button_sheet);
+        buttonOpenBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BootomSheetDialogCamGall bottomsheet = new BootomSheetDialogCamGall();
+                bottomsheet.show(getSupportFragmentManager(), "exemplBottomsheet");
+            }
+        });
+
 
 
     }
 
-
-//****************************************** deb camera
-    private  void  FileChooser(){
-
-            Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(takePic.resolveActivity(getPackageManager())!=null){
-                File photoFile = null;
-
-                    photoFile= createPhotoFile();
-                    if (photoFile !=null){
-
-                         pathToFile=photoFile.getAbsolutePath();
-                        Uri photoUri = FileProvider.getUriForFile(ImagesStorage.this,"com.bouchra.myapplicationechange.fileprovider",photoFile);
-                        takePic.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
-                        startActivityForResult(takePic,PICK_IMAGE_REQUEST);// howa dar 1
-                    }
-
-
-
-            }
-
-
-    }
-
-        private File createPhotoFile() {
-
-            String name= new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            File image=null;
-            try {
-                image = File.createTempFile(name,".jpg",storageDir);
-            } catch (IOException e) {
-                Log.d("Mylog","Excep :"+toString());
-            }
-            return  image;
-        }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            if( resultCode == RESULT_OK)
-            {
-                if(requestCode ==PICK_IMAGE_REQUEST){
-                    Bitmap bitmap= BitmapFactory.decodeFile(pathToFile);
-                    img.setImageBitmap(bitmap);
-                }
-            }
-
-            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                    && data != null && data.getData() != null) {
-
-                mImageUri = data.getData();
-
-                img.setImageURI(mImageUri);
-
-            }
-        }
-    //************************************fin camera
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text =parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    // photo gallery or camera methodes
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    public void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+                    Toast.makeText(getApplicationContext(), "Image Saved!", Toast.LENGTH_SHORT).show();
+                    imageview.setImageBitmap(bitmap);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(thumbnail);
+            saveImage(thumbnail);
+            Toast.makeText(getApplicationContext(), "Image Saved!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(getApplicationContext(),
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::---&gt;" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    private void requestMultiplePermissions() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+                            //openSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<com.karumi.dexter.listener.PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+
+
+
+
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
 }
--- app/src/main/java/com/bouchra/myapplicationechange/adapters/BootomSheetDialogCamGall.java
+++ app/src/main/java/com/bouchra/myapplicationechange/adapters/BootomSheetDialogCamGall.java
@@ -11,33 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.bouchra.myapplicationechange.R;
+import com.bouchra.myapplicationechange.activities.ImagesStorage;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BootomSheetDialogCamGall extends BottomSheetDialogFragment {
+
     private BottomSheetLIstener mListner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.bottom_sheet_camera_gallery,container,false);
+        View v = inflater.inflate(R.layout.bottom_sheet_camera_gallery, container, false);
 
-
-       Button button1=v.findViewById(R.id.btn_camera);
-      Button button2 =v.findViewById(R.id.btn_gallery);
-
-        button1.setOnClickListener(v1 -> {
-            mListner.onButtonCliked("button1 cliked");
-            dismiss();
-
+        Button button1 = v.findViewById(R.id.btn_camera);
+        Button button2 = v.findViewById(R.id.btn_gallery);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getContext(), "button1", Toast.LENGTH_SHORT).show();
+                //   mListner.onButtonCliked("button1 cliked");
+                ((ImagesStorage) getActivity()).takePhotoFromCamera();
+                dismiss();
+            }
         });
-        button2.setOnClickListener(v12 -> {
-            mListner.onButtonCliked("button2 cliked");
-            dismiss();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //   mListner.onButtonCliked("button2 cliked");
+                //  Toast.makeText(getContext(), "button2", Toast.LENGTH_SHORT).show();
+                ((ImagesStorage) getActivity()).choosePhotoFromGallary();
+                dismiss();
+            }
         });
 
-        return  v;
+
+        return v;
     }
-    public  interface BottomSheetLIstener{
-        void  onButtonCliked(String text);
+
+
+    public interface BottomSheetLIstener {
+        void onButtonCliked(String text);
 
     }
 
@@ -45,10 +58,11 @@ public class BootomSheetDialogCamGall extends BottomSheetDialogFragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         try {
-            mListner= (BottomSheetLIstener)context;
-        }catch (ClassCastException e){
-            throw  new ClassCastException(context.toString()+ "doit mettre en oeuvre BottomSheetLIstener");
+            mListner = (BottomSheetLIstener) context;
+        } catch (ClassCastException e) {
+            // throw  new ClassCastException(context.toString()+ "doit mettre en oeuvre BottomSheetLIstener");
         }
 
     }
 }
+
